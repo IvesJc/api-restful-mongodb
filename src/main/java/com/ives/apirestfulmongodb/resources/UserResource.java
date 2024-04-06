dd9ef5b6f1811501ecc642492aed29cf6a5da5d8
Ives Chiba <ivesjundi@gmail.com>
2024-04-06 15:09:01 -0300

    feat: :sparkles: implement get by id method

--- src/main/java/com/ives/apirestfulmongodb/resources/UserResource.java
+++ src/main/java/com/ives/apirestfulmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.ives.apirestfulmongodb.dto.UserDTO;
 import com.ives.apirestfulmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,11 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/ives/apirestfulmongodb/service/UserService.java
+++ src/main/java/com/ives/apirestfulmongodb/service/UserService.java
@@ -2,10 +2,12 @@ package com.ives.apirestfulmongodb.service;
 
 import com.ives.apirestfulmongodb.domain.User;
 import com.ives.apirestfulmongodb.repository.UserRepository;
+import com.ives.apirestfulmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
