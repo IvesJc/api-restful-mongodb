b1d34cf6283bfa9a611fd4e878d8dafad99b3ff6
Ives Chiba <ivesjundi@gmail.com>
2024-04-06 12:55:47 -0300

    init :tada: project created

---
+++ src/main/java/com/ives/apirestfulmongodb/ApiRestfulMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ives.apirestfulmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ives/apirestfulmongodb/ApiRestfulMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ives.apirestfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
