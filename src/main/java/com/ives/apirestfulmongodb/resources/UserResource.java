292efebc63833d1560b33ffee5e37d1abd22c9c8
Ives Chiba <ivesjundi@gmail.com>
2024-04-06 13:54:24 -0300

    feat: :sparkles: implement DTO to return Users

--- src/main/java/com/ives/apirestfulmongodb/resources/UserResource.java
+++ src/main/java/com/ives/apirestfulmongodb/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.ives.apirestfulmongodb.resources;
 
 
 import com.ives.apirestfulmongodb.domain.User;
+import com.ives.apirestfulmongodb.dto.UserDTO;
 import com.ives.apirestfulmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
 
     @RequestMapping(method = RequestMethod.GET)
     // ou @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
