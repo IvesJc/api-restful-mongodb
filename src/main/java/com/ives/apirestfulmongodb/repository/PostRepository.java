07086ad59524ffab6bef1d11606046911154c41c
Ives Chiba <ivesjundi@gmail.com>
2024-04-07 16:03:42 -0300

    feat: :sparkles: consulting with @Query

--- src/main/java/com/ives/apirestfulmongodb/repository/PostRepository.java
+++ src/main/java/com/ives/apirestfulmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.ives.apirestfulmongodb.repository;
 import com.ives.apirestfulmongodb.domain.Post;
 import com.ives.apirestfulmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title':  { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
