f51e7642ca036b06e3d9887bb85d1af103b249b7
Ives Chiba <ivesjundi@gmail.com>
2024-04-06 23:11:16 -0300

    feat: :sparkles: CommentDTO and adding comments to posts

--- src/main/java/com/ives/apirestfulmongodb/config/Instantiation.java
+++ src/main/java/com/ives/apirestfulmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.ives.apirestfulmongodb.config;
 import com.ives.apirestfulmongodb.domain.Post;
 import com.ives.apirestfulmongodb.domain.User;
 import com.ives.apirestfulmongodb.dto.AuthorDTO;
+import com.ives.apirestfulmongodb.dto.CommentDTO;
 import com.ives.apirestfulmongodb.repository.PostRepository;
 import com.ives.apirestfulmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -39,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. " +
                 "Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
