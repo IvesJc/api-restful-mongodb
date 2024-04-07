c4281e6ebb4a057b5dced3110f1798fff0d08e37
Ives Chiba <ivesjundi@gmail.com>
2024-04-06 22:47:46 -0300

    fix: :bug: userRepository saving before Instantiation

--- src/main/java/com/ives/apirestfulmongodb/config/Instantiation.java
+++ src/main/java/com/ives/apirestfulmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.ives.apirestfulmongodb.config;
 
 import com.ives.apirestfulmongodb.domain.Post;
 import com.ives.apirestfulmongodb.domain.User;
+import com.ives.apirestfulmongodb.dto.AuthorDTO;
 import com.ives.apirestfulmongodb.repository.PostRepository;
 import com.ives.apirestfulmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. " +
-                "Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. " +
+                "Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
 
