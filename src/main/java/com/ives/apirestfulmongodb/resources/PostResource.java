5f0d68b2caf9f37a5abe05c660553a2a45584032
Ives Chiba <ivesjundi@gmail.com>
2024-04-07 16:24:50 -0300

    feat: :sparkles: consulting with many requirements

--- src/main/java/com/ives/apirestfulmongodb/resources/PostResource.java
+++ src/main/java/com/ives/apirestfulmongodb/resources/PostResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,18 +25,31 @@ public class PostResource {
     private PostService postService;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Post> findById(@PathVariable String id){
+    public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
     }
 
     @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         // Identifica o que é passado no text, e se nada for passado, será considerado uma String vazia
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
         return ResponseEntity.ok().body(list);
-
     }
 
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        // Identifica o que é passado no text, e se nada for passado, será considerado uma String vazia
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+
+
+    }
 }
